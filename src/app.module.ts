b5c597bb9a5ffbb53fd80040deff10e213c63d76
m.koponirov <m.koponirov@cdek.ru>
2022-01-06 18:51:41 +0700

    serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ArticleModule } from './article/article.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://superuser1:superuser1@cluster0.soxoc.mongodb.net/blog?retryWrites=true&w=majority',
     ),
